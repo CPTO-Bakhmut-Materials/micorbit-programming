710a562a1f75315a8a3fa9c282bcb6e5c3275c7e
vperus <vperus@gmail.com>
2025-12-10 17:34:05 +0200

    [analog] Added led continuous update

--- src/analog.py
+++ src/analog.py
@@ -1,8 +1,11 @@
-point_x = 2
-point_y = 2
+TICK_TIME = 1000                    # Змінна - час очікування
+point_x = 2                         # Змінна - X координата
+point_y = 2                         # Змінна - Y координата
 
-led.plot(point_x, point_y)
+led.plot(point_x, point_y)          # Засвітити діод на позиції
 
-def on_forever():
-    pass
-forever(on_forever)
+def on_forever():                   # Оголошуємо функцію яка міститиме наш головний код
+    led.unplot(point_x, point_y)    # Виключити діод
+    led.plot(point_x, point_y)      # Ввімкнути діод
+    pause(TICK_TIME)                # Очікуємо 1000 мілісекунд
+forever(on_forever)                 # Говоримо компютеру щоб він постійно викликав функцію
